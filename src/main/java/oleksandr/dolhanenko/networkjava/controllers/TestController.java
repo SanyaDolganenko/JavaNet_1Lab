b2e51cfd0996d036e8005db35dc3633c8828b907
Sasha Dolganenko <sasha.dolganenko@gmail.com>
2019-04-25 22:34:55 +0300

    Working updating computer.

--- src/main/java/oleksandr/dolhanenko/networkjava/controllers/TestController.java
+++ src/main/java/oleksandr/dolhanenko/networkjava/controllers/TestController.java
@@ -1,12 +1,13 @@
 package oleksandr.dolhanenko.networkjava.controllers;
 
+import oleksandr.dolhanenko.networkjava.model.Computer;
 import oleksandr.dolhanenko.networkjava.utils.ComputerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/test")
@@ -18,5 +19,22 @@ public class TestController {
     public String messages(Model model) {
         model.addAttribute("computers", computerRepository.findAll());
         return "computers";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String greeting(@PathVariable("id") int id, Model model) {
+        model.addAttribute("computer", computerRepository.getOne(id));
+        return "computer-edit";
+    }
+
+    @PostMapping("/update")
+    public String updateComputer(Computer computer) {
+        if (computer != null) {
+            if (computerRepository.findById(computer.getId()).isPresent()) {
+                computerRepository.save(computer);
+            }
+        }
+
+        return "redirect:computers";
     }
 }
