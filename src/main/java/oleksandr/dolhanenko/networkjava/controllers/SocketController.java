0fb83926f6a0abfdad50e06ae63efe2ce6a621cb
Sasha Dolganenko <sasha.dolganenko@gmail.com>
2019-05-07 20:59:28 +0300

    Working delete and get by websocket.

--- src/main/java/oleksandr/dolhanenko/networkjava/controllers/SocketController.java
+++ src/main/java/oleksandr/dolhanenko/networkjava/controllers/SocketController.java
@@ -25,32 +25,70 @@ public class SocketController {
 
 
     private Message processRequest(String request) {
-//        Message resultMessage = new Message();
-//        resultMessage.setMessage("Requested: " +
-//                HtmlUtils.htmlEscape(message.getMessage()));
         Message message = new Message();
         message.setMessage("Unknown query");
+        int number = getEnteredNumber(request);
+        Computer computer = null;
+        if (number == -1) {
+            message.setMessage("Invalid number");
+        } else {
+            computer = getComputerSafe(number);
+            if (computer == null) {
+                message.setMessage("No computer found with this id");
+            }
+        }
         if (request.contains("GET")) {
-            int indexStart = request.indexOf("(");
-            int indexEnd = request.indexOf(")");
-            if (indexStart >= 0 && indexEnd >= 0 && indexEnd > indexStart) {
-                String numberString = request.substring(indexStart + 1, indexEnd);
-                try {
-                    int number = Integer.parseInt(numberString);
-                    Computer computer = computerRepository.findById(number).get();
-                    ObjectMapper mapper = new ObjectMapper();
-                    String json = mapper.writeValueAsString(computer);
-                    message.setMessage(json);
-                } catch (NoSuchElementException e) {
-                    message.setMessage("No computer found with this id");
-                } catch (NumberFormatException e) {
-                    message.setMessage("Invalid number");
-                } catch (JsonProcessingException e) {
-                    e.printStackTrace();
+            if (computer != null) {
+                message.setMessage(computerToJsonSafe(computer));
+            }
+        } else if (request.contains("DELETE")) {
+            if (computer != null) {
+                long allCount = computerRepository.count();
+                computerRepository.delete(computer);
+                if (allCount - computerRepository.count() == 1) {
+                    message.setMessage("Successfully deleted computer");
+                }else{
+                    message.setMessage("Error deleting computer");
                 }
             }
         }
         return message;
+    }
+
+    private Computer getComputerSafe(int id) {
+        Computer computer = null;
+        try {
+            computer = computerRepository.findById(id).get();
+        } catch (NoSuchElementException e) {
+
+        }
+        return computer;
+    }
+
+    private String computerToJsonSafe(Computer computer) {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            String json = mapper.writeValueAsString(computer);
+            return json;
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return "Error serializing.";
+    }
+
+    private int getEnteredNumber(String request) {
+        int indexStart = request.indexOf("(");
+        int indexEnd = request.indexOf(")");
+        int number = -1;
+        if (indexStart >= 0 && indexEnd >= 0 && indexEnd > indexStart) {
+            String numberString = request.substring(indexStart + 1, indexEnd);
+            try {
+                number = Integer.parseInt(numberString);
+            } catch (NumberFormatException e) {
+                number = -1;
+            }
+        }
+        return number;
     }
 
 }
